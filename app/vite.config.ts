bbfb744ab0739288b6a485dae07f6046ace5a4b3
Thamadie Gunawardena <thamadie13@gmail.com>
2026-06-04 14:07:25 +0530

    updated route

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -17,6 +17,6 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 //   },
 // });
 export default defineConfig({
-  base: '/thamadie-portfolio/', // Must match your GitHub repo name exactly
+  base: '/thamadie-portfolio/app/index.html', // Must match your GitHub repo name exactly
   plugins: [inspectAttr(), react()],
 })
