641404e6c008926ed4c727798550b15925559bdf
Thamadie Gunawardena <thamadie13@gmail.com>
2026-06-04 13:59:46 +0530

    updated vite coonfig file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,15 +4,19 @@ import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   base: './',
+//   plugins: [inspectAttr(), react()],
+//   server: {
+//     port: 3000,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
 export default defineConfig({
-  base: './',
+  base: '/thamadie-portfolio/', // Must match your GitHub repo name exactly
   plugins: [inspectAttr(), react()],
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+})
